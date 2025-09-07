008418c418e99f09b03adcdef1b7aaa349c924e7
Мария Дёмина <deminam2017@gmail.com>
2025-09-08 02:35:26 +0300

    Initial commit of Django project

--- uwsgi/run_uwsgi.sh
+++ uwsgi/run_uwsgi.sh
@@ -11,6 +11,10 @@ python manage.py shell -c "from django.db import connection; cursor = connection
 echo "Applying database migrations..."
 python manage.py migrate
 
+# Сбор статических файлов
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 # Запуск сервера uWSGI
 echo "Starting server..."
 exec uwsgi --strict --ini uwsgi.ini
